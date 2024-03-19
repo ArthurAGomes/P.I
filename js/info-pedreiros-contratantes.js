382084c45d133b267bb38e17f37d7b8d44f58f15
Yago <yagoabscosta@gmail.com>
2024-03-19 18:10:05 -0300

    pag seja parceiro, dados de pedreiros e contratantes no JS

---
+++ js/info-pedreiros-contratantes.js
@@ -0,0 +1,67 @@
+let pedreiros = [
+    {
+        "nome": "Joaquim Barbosa",
+        "cpf": 46687990945,
+        "senha": 72485689,
+        "cep": "02856030",
+        "pagamento": "Pix, Dinheiro",
+        "dias": "Sabado e/ou Domingo",
+        "telefone": 11958769028 
+    },
+    {
+        "nome": "Joaquim Barbosa",
+        "cpf": 46687990945,
+        "senha": 72485689,
+        "cep": "02856030",
+        "pagamento": "Pix, Dinheiro",
+        "dias": "Sabado e/ou Domingo",
+        "telefone": 11958769028 
+    },
+    {
+        "nome": "Joaquim Barbosa",
+        "cpf": 46687990945,
+        "senha": 72485689,
+        "cep": "02856030",
+        "pagamento": "Pix, Dinheiro",
+        "dias": "Sabado e/ou Domingo",
+        "telefone": 11958769028 
+    },
+    {
+        "nome": "Joaquim Barbosa",
+        "cpf": 46687990945,
+        "senha": 72485689,
+        "cep": "02856030",
+        "pagamento": "Pix, Dinheiro",
+        "dias": "Sabado e/ou Domingo",
+        "telefone": 11958769028 
+    },
+    {
+        "nome": "Joaquim Barbosa",
+        "cpf": 46687990945,
+        "senha": 72485689,
+        "cep": "02856030",
+        "pagamento": "Pix, Dinheiro",
+        "dias": "Sabado e/ou Domingo",
+        "telefone": 11958769028 
+    },
+    {
+        "nome": "Joaquim Barbosa",
+        "cpf": 46687990945,
+        "senha": 72485689,
+        "cep": "02856030",
+        "pagamento": "Pix, Dinheiro",
+        "dias": "Sabado e/ou Domingo",
+        "telefone": 11958769028 
+    },
+]
+
+let contratantes = [
+    {
+        "nome": "Casimiro Miguel",
+        "senha": "123vascao@",
+        "cpf": "07734929345",
+        "genero": "Homem",
+        "cep": "02545876", 
+        "foto1": "perfil.img"
+    }
+]
