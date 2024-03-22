ebbd362b4082755df6969009dc6f7548c045dbe3
YagoABSC <yagoabscosta@gmail.com>
2024-03-21 22:20:00 -0300

    area pedreiro mais coisas

--- js/info-pedreiros-contratantes.js
+++ js/info-pedreiros-contratantes.js
@@ -1,107 +1,107 @@
 let pedreiros = [
     {
-        "nome": "Joaquim Barbosa",
-        "cpf": 46687990945,
-        "senha": 72485689,
-        "cep": "02856030",
-        "pagamento": "Pix, Dinheiro",
-        "dias": "Sabado e/ou Domingo",
-        "telefone": 11958769028 
+        nome: "Joaquim Barbosa",
+        cpf: 46687990945,
+        senha: 72485689,
+        cep: "02856030",
+        pagamento: "Pix, Dinheiro",
+        dias: "Sabado e/ou Domingo",
+        telefone: 11958769028 
     },
     {
-        "nome": "Matheus Oliveira",
-        "cpf": 46687990945,
-        "senha": 72485689,
-        "cep": "02856030",
-        "pagamento": "Pix, Crédito",
-        "dias": "Todos os dias",
-        "telefone": 11958769028 
+        nome: "Matheus Oliveira",
+        cpf: 46687990945,
+        senha: 72485689,
+        cep: "02856030",
+        pagamento: "Pix, Crédito",
+        dias: "Todos os dias",
+        telefone: 11958769028 
     },
     {
-        "nome": "Joaquim Barbosa",
-        "cpf": 46687990945,
-        "senha": 72485689,
-        "cep": "02856030",
-        "pagamento": "Pix, Dinheiro",
-        "dias": "Sabado e/ou Domingo",
-        "telefone": 11958769028 
+        nome: "Joaquim Barbosa",
+        cpf: 46687990945,
+        senha: 72485689,
+        cep: "02856030",
+        pagamento: "Pix, Dinheiro",
+        dias: "Sabado e/ou Domingo",
+        telefone: 11958769028 
     },
     {
-        "nome": "Joaquim Barbosa",
-        "cpf": 46687990945,
-        "senha": 72485689,
-        "cep": "02856030",
-        "pagamento": "Pix, Dinheiro",
-        "dias": "Sabado e/ou Domingo",
-        "telefone": 11958769028 
+        nome: "Joaquim Barbosa",
+        cpf: 46687990945,
+        senha: 72485689,
+        cep: "02856030",
+        pagamento: "Pix, Dinheiro",
+        dias: "Sabado e/ou Domingo",
+        telefone: 11958769028 
     },
     {
-        "nome": "Joaquim Barbosa",
-        "cpf": 46687990945,
-        "senha": 72485689,
-        "cep": "02856030",
-        "pagamento": "Pix, Dinheiro",
-        "dias": "Sabado e/ou Domingo",
-        "telefone": 11958769028 
+        nome: "Joaquim Barbosa",
+        cpf: 46687990945,
+        senha: 72485689,
+        cep: "02856030",
+        pagamento: "Pix, Dinheiro",
+        dias: "Sabado e/ou Domingo",
+        telefone: 11958769028 
     },
     {
-        "nome": "Joaquim Barbosa",
-        "cpf": 46687990945,
-        "senha": 72485689,
-        "cep": "02856030",
-        "pagamento": "Pix, Dinheiro",
-        "dias": "Sabado e/ou Domingo",
-        "telefone": 11958769028 
+        nome: "Joaquim Barbosa",
+        cpf: 46687990945,
+        senha: 72485689,
+        cep: "02856030",
+        pagamento: "Pix, Dinheiro",
+        dias: "Sabado e/ou Domingo",
+        telefone: 11958769028 
     },
 ]
 
 let contratantes = [
     {
-        "nome": "Casimiro Miguel",
-        "senha": "123vascao@",
-        "cpf": "07734929345",
-        "genero": "Homem",
-        "cep": "02545876", 
-        "foto1": "perfil.img"
+        nome: "Casimiro Miguel",
+        senha: "123vascao@",
+        cpf: "07734929345",
+        genero: "Homem",
+        cep: "02545876", 
+        fotoPerfil: "perfil.img"
     },
     {
-        "nome": "Casimiro Miguel",
-        "senha": "123vascao@",
-        "cpf": "07734929345",
-        "genero": "Homem",
-        "cep": "02545876", 
-        "foto1": "perfil.img"
+        nome: "Casimiro Miguel",
+        senha: "123vascao@",
+        cpf: "07734929345",
+        genero: "Homem",
+        cep: "02545876", 
+        fotoPerfil: "perfil.img"
     },
     {
-        "nome": "Casimiro Miguel",
-        "senha": "123vascao@",
-        "cpf": "07734929345",
-        "genero": "Homem",
-        "cep": "02545876", 
-        "foto1": "perfil.img"
+        nome: "Casimiro Miguel",
+        senha: "123vascao@",
+        cpf: "07734929345",
+        genero: "Homem",
+        cep: "02545876", 
+        fotoPerfil: "perfil.img"
     },
     {
-        "nome": "Casimiro Miguel",
-        "senha": "123vascao@",
-        "cpf": "07734929345",
-        "genero": "Homem",
-        "cep": "02545876", 
-        "foto1": "perfil.img"
+        nome: "Casimiro Miguel",
+        senha: "123vascao@",
+        cpf: "07734929345",
+        genero: "Homem",
+        cep: "02545876", 
+        fotoPerfil: "perfil.img"
     },
     {
-        "nome": "Casimiro Miguel",
-        "senha": "123vascao@",
-        "cpf": "07734929345",
-        "genero": "Homem",
-        "cep": "02545876", 
-        "foto1": "perfil.img"
+        nome: "Casimiro Miguel",
+        senha: "123vascao@",
+        cpf: "07734929345",
+        genero: "Homem",
+        cep: "02545876", 
+        fotoPerfil: "perfil.img"
     },
     {
-        "nome": "Casimiro Miguel",
-        "senha": "123vascao@",
-        "cpf": "07734929345",
-        "genero": "Homem",
-        "cep": "02545876", 
-        "foto1": "perfil.img"
+        nome: "Casimiro Miguel",
+        senha: "123vascao@",
+        cpf: "07734929345",
+        genero: "Homem",
+        cep: "02545876", 
+        fotoPerfil: "perfil.img"
     }
 ]
