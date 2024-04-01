c0e346721f474c1f0aed39a3939f734ec1aa76c1
Yago <yagoabscosta@gmail.com>
2024-04-01 18:13:17 -0300

    marcadores funcionando

--- js/info-pedreiros-contratantes.js
+++ js/info-pedreiros-contratantes.js
@@ -1,122 +1,130 @@
-let pedreiros = [
-    {
-        nome: "Joaquim Barbosa",
-        cpf: 46687990945,
-        senha: "senha", //https://emn178.github.io/online-tools/sha256.html
-        cep: "02856030",
-        pagamento: "Pix, Dinheiro",
-        dias: "Sabado e/ou Domingo",
-        telefone: 11958769028 
-    },
-    {
-        nome: "Matheus Oliveira",
-        cpf: 46687990945,
-        senha: 72485689,
-        cep: "02856030",
-        pagamento: "Pix, Crédito",
-        dias: "Todos os dias",
-        telefone: 11958769028 
-    },
-    {
-        nome: "Joaquim Barbosa",
-        cpf: 46687990945,
-        senha: 72485689,
-        cep: "02856030",
-        pagamento: "Pix, Dinheiro",
-        dias: "Sabado e/ou Domingo",
-        telefone: 11958769028 
-    },
-    {
-        nome: "Joaquim Barbosa",
-        cpf: 46687990945,
-        senha: 72485689,
-        cep: "02856030",
-        pagamento: "Pix, Dinheiro",
-        dias: "Sabado e/ou Domingo",
-        telefone: 11958769028 
-    },
-    {
-        nome: "Joaquim Barbosa",
-        cpf: 46687990945,
-        senha: 72485689,
-        cep: "02856030",
-        pagamento: "Pix, Dinheiro",
-        dias: "Sabado e/ou Domingo",
-        telefone: 11958769028 
-    },
-    {
-        nome: "Joaquim Barbosa",
-        cpf: 46687990945,
-        senha: 72485689,
-        cep: "02856030",
-        pagamento: "Pix, Dinheiro",
-        dias: "Sabado e/ou Domingo",
-        telefone: 11958769028 
-    },
-]
-
-// let contratantes = [
+// let pedreiros = [
 //     {
-//         nome: "Casimiro Miguel",
-//         senha: "123vascao@",
-//         cpf: "07734929345",
-//         genero: "Homem",
-//         cep: "02545876",
-//         lat: "-23.440222219466328",
-//         lng: "-46.749272201600995",
-//         fotoPerfil: "perfil.img"
+//         nome: "Joaquim Barbosa",
+//         cpf: 46687990945,
+//         senha: "senha", //https://emn178.github.io/online-tools/sha256.html
+//         cep: "02856030",
+//         pagamento: "Pix, Dinheiro",
+//         dias: "Sabado e/ou Domingo",
+//         telefone: 11958769028 
 //     },
 //     {
-//         nome: "Luva de Pedreiro",
-//         senha: "123vascao@",
-//         cpf: "07734929345",
-//         genero: "Homem",
-//         cep: "02545876", 
-//         lat: "-23.449356620756024",
-//         lng: "-46.73176274150261",
-//         fotoPerfil: "perfil.img"
+//         nome: "Matheus Oliveira",
+//         cpf: 46687990945,
+//         senha: 72485689,
+//         cep: "02856030",
+//         pagamento: "Pix, Crédito",
+//         dias: "Todos os dias",
+//         telefone: 11958769028 
 //     },
 //     {
-//         nome: "Gaules",
-//         senha: "123vascao@",
-//         cpf: "07734929345",
-//         genero: "Homem",
-//         cep: "02545876", 
-//         lat: "-23.45140389991784",
-//         lng: "-46.734680984852346",
-//         fotoPerfil: "perfil.img"
+//         nome: "Joaquim Barbosa",
+//         cpf: 46687990945,
+//         senha: 72485689,
+//         cep: "02856030",
+//         pagamento: "Pix, Dinheiro",
+//         dias: "Sabado e/ou Domingo",
+//         telefone: 11958769028 
 //     },
 //     {
-//         nome: "Baiano",
-//         senha: "123vascao@",
-//         cpf: "07734929345",
-//         genero: "Homem",
-//         cep: "02545876",
-//         lat: "-23.451246418032003",
-//         lng: "-46.736912582708015", 
-//         fotoPerfil: "perfil.img"
+//         nome: "Joaquim Barbosa",
+//         cpf: 46687990945,
+//         senha: 72485689,
+//         cep: "02856030",
+//         pagamento: "Pix, Dinheiro",
+//         dias: "Sabado e/ou Domingo",
+//         telefone: 11958769028 
 //     },
 //     {
-//         nome: "Casimiro Miguel",
-//         senha: "123vascao@",
-//         cpf: "07734929345",
-//         genero: "Homem",
-//         cep: "02545876",
-//         lat: "-23.451482640790346",
-//         lng: "-46.7379425509491", 
-//         fotoPerfil: "perfil.img"
+//         nome: "Joaquim Barbosa",
+//         cpf: 46687990945,
+//         senha: 72485689,
+//         cep: "02856030",
+//         pagamento: "Pix, Dinheiro",
+//         dias: "Sabado e/ou Domingo",
+//         telefone: 11958769028 
 //     },
 //     {
-//         nome: "Casimiro Miguel",
-//         senha: "123vascao@",
-//         cpf: "07734929345",
-//         genero: "Homem",
-//         cep: "02545876", 
-//         lat: "-23.454396020070924",
-//         lng: "-46.74669728099829",
-//         fotoPerfil: "perfil.img"
-//     }
+//         nome: "Joaquim Barbosa",
+//         cpf: 46687990945,
+//         senha: 72485689,
+//         cep: "02856030",
+//         pagamento: "Pix, Dinheiro",
+//         dias: "Sabado e/ou Domingo",
+//         telefone: 11958769028 
+//     },
 // ]
+
+let contratantes = [
+  {
+    nome: "Yago Aparecido B. S. Costa",
+    cpf: "077.349.293-45",
+    cel: "11957069027",
+    genero: "Homem",
+    cep: "02984050",
+    lat: "-23.440222219466328",
+    lng: "-46.749272201600995",
+    fotoPerfil: "https://pbs.twimg.com/profile_images/1159853984963866626/50dRzHVk_400x400.jpg",
+    uf: "SP",
+    cidade: "São Paulo",
+    complemento: 178
+  },
+  {
+    nome: "Luva de Pedreiro",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    cel: "4002-8922",
+    genero: "Homem",
+    cep: "02545876",
+    lat: "-23.449356620756024",
+    lng: "-46.73176274150261",
+    fotoPerfil: "perfil.img"
+  },
+  {
+    nome: "Gaules",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    cel: "4002-8922",
+    genero: "Homem",
+    cep: "02545876",
+    lat: "-23.45140389991784",
+    lng: "-46.734680984852346",
+    fotoPerfil: "perfil.img"
+  },
+  {
+    nome: "Baiano",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    cel: "4002-8922",
+    genero: "Homem",
+    cep: "02545876",
+    lat: "-23.451246418032003",
+    lng: "-46.736912582708015",
+    fotoPerfil: "perfil.img"
+  },
+  {
+    nome: "Casimiro Miguel",
+    senha: "123vascao@",
+    cpf: "077.349.293-45",
+    cel: "4002-8922",
+    genero: "Homem",
+    cep: "02545876",
+    lat: "-23.451482640790346",
+    lng: "-46.7379425509491",
+    fotoPerfil: "perfil.img"
+  },
+  {
+    nome: "Casimiro Miguel",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    cel: "4002-8922",
+    genero: "Homem",
+    cep: "02545876",
+    lat: "-23.454396020070924",
+    lng: "-46.74669728099829",
+    fotoPerfil: "perfil.img"
+  }
+]
 
 // ------------------- MAPA
 
@@ -124,64 +132,70 @@ let map, infoWindow;
 
 const locations = [
   {
-      nome: "Casimiro Miguel",
-      senha: "123vascao@",
-      cpf: "07734929345",
-      genero: "Homem",
-      cep: "02545876",
-      lat: -23.440222219466328,
-      lng: -46.749272201600995,
-      fotoPerfil: "perfil.img"
+    nome: "Casimiro Miguel",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    genero: "Homem",
+    cep: "02545876",
+    lat: -23.440222219466328,
+    lng: -46.749272201600995,
+    fotoPerfil: "perfil.img",
+    cel: 11957069027
   },
   {
-      nome: "Luva de Pedreiro",
-      senha: "123vascao@",
-      cpf: "07734929345",
-      genero: "Homem",
-      cep: "02545876", 
-      lat: -23.449356620756024,
-      lng: -46.73176274150261,
-      fotoPerfil: "perfil.img"
+    nome: "Luva de Pedreiro",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    genero: "Homem",
+    cep: "02545876",
+    lat: -23.449356620756024,
+    lng: -46.73176274150261,
+    fotoPerfil: "perfil.img",
+    cel: 11957069027
   },
   {
-      nome: "Gaules",
-      senha: "123vascao@",
-      cpf: "07734929345",
-      genero: "Homem",
-      cep: "02545876", 
-      lat: -23.45140389991784,
-      lng: -46.734680984852346,
-      fotoPerfil: "perfil.img"
+    nome: "Gaules",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    genero: "Homem",
+    cep: "02545876",
+    lat: -23.45140389991784,
+    lng: -46.734680984852346,
+    fotoPerfil: "perfil.img",
+    cel: 11957069027
   },
   {
-      nome: "Baiano",
-      senha: "123vascao@",
-      cpf: "07734929345",
-      genero: "Homem",
-      cep: "02545876",
-      lat: -23.451246418032003,
-      lng: -46.736912582708015, 
-      fotoPerfil: "perfil.img"
+    nome: "Baiano",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    genero: "Homem",
+    cep: "02545876",
+    lat: -23.451246418032003,
+    lng: -46.736912582708015,
+    fotoPerfil: "perfil.img",
+    cel: 11957069027
   },
   {
-      nome: "Casimiro Miguel",
-      senha: "123vascao@",
-      cpf: "07734929345",
-      genero: "Homem",
-      cep: "02545876",
-      lat: -23.451482640790346,
-      lng: -46.7379425509491, 
-      fotoPerfil: "perfil.img"
+    nome: "Casimiro Miguel",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    genero: "Homem",
+    cep: "02545876",
+    lat: -23.451482640790346,
+    lng: -46.7379425509491,
+    fotoPerfil: "perfil.img",
+    cel: 11957069027
   },
   {
-      nome: "Casimiro Miguel",
-      senha: "123vascao@",
-      cpf: "07734929345",
-      genero: "Homem",
-      cep: "02545876", 
-      lat: -23.454396020070924,
-      lng: -46.74669728099829,
-      fotoPerfil: "perfil.img"
+    nome: "Casimiro Miguel",
+    senha: "123vascao@",
+    cpf: "07734929345",
+    genero: "Homem",
+    cep: "02545876",
+    lat: -23.454396020070924,
+    lng: -46.74669728099829,
+    fotoPerfil: "perfil.img",
+    cel: 11957069027
   }
 ];
 
@@ -193,7 +207,7 @@ async function initMap() {
 
   map = new Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 15, 
+    zoom: 15,
   });
 
   infoWindow = new google.maps.InfoWindow();
@@ -220,35 +234,75 @@ async function initMap() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // Add some markers to the map.
-    const markers = locations.map((position, i) => {
-    const label = labels[i % labels.length];
+  // const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  const markers = locations.map((position, i) => {
+    const label = locations[i % locations.length];
     const marker = new google.maps.Marker({
       position,
       label,
     });
     marker.addListener("click", () => {
-        infoWindow.setContent(label);
-        infoWindow.open(map, marker);
-      });
-      return marker;
+      infoWindow.setContent(
+        `<p>${locations[i].nome}</p>
+        <p>${locations[i].genero}</p>
+        <a href="https://wa.me/${locations[i].cel}">
+          <button>Entrar em contato</button>
+        </a>
+        `);
+      infoWindow.open(map, marker);
     });
-    new MarkerClusterer({ markers, map });
+    return marker;
+  });
+  new MarkerClusterer({ markers, map });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(
-      browserHasGeolocation
-        ? "Error: The Geolocation service failed."
-        : "Error: Your browser doesn't support geolocation."
-    );
-    infoWindow.open(map);
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 
 initMap();
 
-function perfilContratante(){
+// Area contratante
 
-}
+$.ajax({
+  "url": "http://cep.republicavirtual.com.br/web_cep.php",
+
+  "type": "GET",
+
+  "dataType": "JSON",
+
+  "data": "cep=" + contratantes[0].cep + "&formato=JSON",
+
+  "beforeSend": () => { },
+
+  "success": (resposta) => {
+
+    document.getElementById("nome-contratante").value = contratantes[0].nome
+
+    document.getElementById("cpf-contratante").value = contratantes[0].cpf
+
+    document.getElementById("cel-contratante").value = contratantes[0].cel
+
+    document.getElementById("cep-contratante").value = contratantes[0].cep
+
+    document.getElementById("cidade-contratante").value = resposta.cidade
+
+    document.getElementById("uf-contratante").value = resposta.uf
+
+    document.getElementById("rua-contratante").value = resposta.logradouro
+
+    document.getElementById("bairro-contratante").value = resposta.bairro
+
+    document.getElementById("complemento-contratante").value = contratantes[0].complemento
+
+
+
+  }
+})
