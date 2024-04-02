22eeff3b2a5ac809fcdc9f80fc5bd80f2c8d715d
YagoABSC <yagoabscosta@gmail.com>
2024-04-01 22:30:03 -0300

    jajajajaj

--- js/info-pedreiros-contratantes.js
+++ js/info-pedreiros-contratantes.js
@@ -133,69 +133,81 @@ let map, infoWindow;
 const locations = [
   {
     nome: "Casimiro Miguel",
-    senha: "123vascao@",
+    // senha: "123vascao@", 
     cpf: "07734929345",
     genero: "Homem",
     cep: "02545876",
     lat: -23.440222219466328,
     lng: -46.749272201600995,
-    fotoPerfil: "perfil.img",
-    cel: 11957069027
+    fotoPerfil: "https://conteudo.imguol.com.br/c/entretenimento/17/2022/11/07/casimiro-miguel-faz-lives-que-duram-8-horas-1667815375225_v2_900x506.jpg",
+    cel: 11957069027,
+    diasDispo: "Sábado, e/ou Domingo",
+    tiposTrab: "Obras gerais"
   },
   {
     nome: "Luva de Pedreiro",
-    senha: "123vascao@",
+    // senha: "123vascao@", 
     cpf: "07734929345",
     genero: "Homem",
     cep: "02545876",
     lat: -23.449356620756024,
     lng: -46.73176274150261,
-    fotoPerfil: "perfil.img",
-    cel: 11957069027
+    fotoPerfil: "https://s2.glbimg.com/Dphh0soP3oi1VEq1fQLv21GxO4M=/smart/e.glbimg.com/og/ed/f/original/2022/03/15/iran_ferreira.png",
+    cel: 11957069027,
+    diasDispo: "Todos",
+    tiposTrab: "Pequenas reformas"
   },
   {
     nome: "Gaules",
-    senha: "123vascao@",
+    // senha: "123vascao@", 
     cpf: "07734929345",
     genero: "Homem",
     cep: "02545876",
     lat: -23.45140389991784,
     lng: -46.734680984852346,
-    fotoPerfil: "perfil.img",
-    cel: 11957069027
+    fotoPerfil: "https://s2-ge.glbimg.com/zyt4LdaXy_eH1c4DVKB7Yvgm3qM=/0x0:2570x1928/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/K/f/BSvir8QJaQnIUA5CzB5Q/gau.jpeg",
+    cel: 11957069027,
+    diasDispo: "Segund,a á Sexta",
+    tiposTrab: "Obras gerais"
   },
   {
     nome: "Baiano",
-    senha: "123vascao@",
+    // senha: "123vascao@", 
     cpf: "07734929345",
     genero: "Homem",
     cep: "02545876",
     lat: -23.451246418032003,
     lng: -46.736912582708015,
-    fotoPerfil: "perfil.img",
-    cel: 11957069027
+    fotoPerfil: "https://s2-ge.glbimg.com/ivP3Ca2LCv6OAfvu-oXJXfVK_9c=/0x0:960x640/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/u/f/WCMxWrT4qZsAcBXqlFzg/baiano.jpeg",
+    cel: 11957069027,
+    diasDispo: "Todos",
+    tiposTrab: "Reformas gerais"
   },
   {
-    nome: "Casimiro Miguel",
-    senha: "123vascao@",
+    nome: "Elenilda Santos",
+    // senha: "123vascao@", 
     cpf: "07734929345",
-    genero: "Homem",
+    genero: "Mulher",
     cep: "02545876",
     lat: -23.451482640790346,
     lng: -46.7379425509491,
-    fotoPerfil: "perfil.img",
-    cel: 11957069027
+    fotoPerfil: "https://sintricomb.com.br/wp-content/uploads/2020/08/Elenilda-4.jpg",
+    cel: 11957069027,
+    diasDispo: "Todos",
+    tiposTrab: "Obras gerais"
   },
   {
-    nome: "Casimiro Miguel",
-    senha: "123vascao@",
+    nome: "Eliete Vieira Barbosa",
+    // senha: "123vascao@", 
     cpf: "07734929345",
-    genero: "Homem",
+    genero: "Mulher",
     cep: "02545876",
     lat: -23.454396020070924,
     lng: -46.74669728099829,
-    fotoPerfil: "perfil.img",
-    cel: 11957069027
+    fotoPerfil: "https://s2.glbimg.com/r1gj-G1q9dDvivLPwgkfTSD3nhI=/620x345/e.glbimg.com/og/ed/f/original/2019/02/19/liaaa.png",
+    cel: 11957069027,
+    diasDispo: "Segunda á Sexta",
+    tiposTrab: "Pequenas reformas"
   }
 ];
 
@@ -244,11 +256,18 @@ async function initMap() {
     });
     marker.addListener("click", () => {
       infoWindow.setContent(
-        `<p>${locations[i].nome}</p>
-        <p>${locations[i].genero}</p>
-        <a href="https://wa.me/${locations[i].cel}">
-          <button>Entrar em contato</button>
-        </a>
+        `<div id="card-mapa-perfil">
+          <img src="${locations[i].fotoPerfil}" alt="${locations[i].nome}">
+          <div>
+            <h3>${locations[i].nome}</h3>
+            <p>Dias diponíveis: ${locations[i].diasDispo}</p>
+            <p>Tipos de trabalho: ${locations[i].tiposTrab}</p>
+          </div>
+
+          <a href="https://wa.me/${locations[i].cel}">
+            <button>Entrar em contato</button>
+          </a>
+        </div>
         `);
       infoWindow.open(map, marker);
     });
